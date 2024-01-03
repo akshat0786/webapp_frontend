a7082fc985b8fdbce14101b8de044e2c7d333c28
akshat <akshatroljack@gmail.com>
2024-01-03 18:01:34 +0530

    3 jan

--- src/app/components/main-header/main-header.component.ts
+++ src/app/components/main-header/main-header.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import intlTelInput from 'intl-tel-input';
 declare var $: any;
 @Component({
   selector: 'app-main-header',
@@ -8,10 +8,18 @@ declare var $: any;
   templateUrl: './main-header.component.html',
   styleUrl: './main-header.component.css',
 })
-export class MainHeaderComponent {
-  // openLogin() {
-  // console.log('in fun');
-  // $('#basicModal').modal();
-  // console.log($('#basicModal'), 'modal');
-  // }
-}
+export class MainHeaderComponent {}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const phoneInput = document.getElementById('phone');
+
+  // Check if phoneInput is not null before using it
+  if (phoneInput) {
+    intlTelInput(phoneInput as HTMLInputElement, {
+      initialCountry: 'in',
+      separateDialCode: true,
+    });
+  } else {
+    console.error('Element with id "phone" not found.');
+  }
+});
