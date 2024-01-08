3211246fa17679ed5efff18344661fe58b3f8fd1
akshat <akshatroljack@gmail.com>
2024-01-08 18:12:41 +0530

    8 jan

--- src/app/components/main-header/main-header.component.ts
+++ src/app/components/main-header/main-header.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import intlTelInput from 'intl-tel-input';
 import { ApiService } from '../../services/api.services';
 import { Router } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -11,46 +10,19 @@ declare var $: any;
   imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './main-header.component.html',
   styleUrl: './main-header.component.css',
+  providers:[ApiService]
 })
 export class MainHeaderComponent {
-  constructor(public ApiService: ApiService, public router: Router) {
+
+  public phone:any;
+  public otp: any;
+
+  constructor(public apiService: ApiService, public router: Router) {
     let token = localStorage.getItem('token');
     if (token) {
       this.router.navigate(['home']);
     }
   }
-
-  onSubmit(FormData: any) {
-    this.ApiService.login(FormData).subscribe({
-      next: (v) => {
-        console.log(v);
-        localStorage.setItem('userdata', JSON.stringify(v.user));
-        localStorage.setItem('token', v.token);
-        this.router.navigate(['home']);
-      },
-      error: (e) => {
-        console.error(e);
-      },
-      complete: () => console.info('complete'),
-    });
-  }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  const phoneInput = document.getElementById('phone');
 
-  // Check if phoneInput is not null before using it
-  if (phoneInput) {
-    intlTelInput(phoneInput as HTMLInputElement, {
-      initialCountry: 'in',
-      separateDialCode: true,
-    });
-  } else {
-    console.error('Element with id "phone" not found.');
-  }
-});
-
-// ***********Script to open modal by default*********
-// $(window).load(function () {
-//   $('#loginModal').modal('show');
-// });
--- src/app/services/api.services.ts
+++ src/app/services/api.services.ts
@@ -2,22 +2,27 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { environment } from "../../environment/environment";
 import { Router } from "@angular/router";
 import { Observable } from "rxjs";
+// import { Injectable } from "@angular/core";
 
+// @Injectable()
 export class ApiService {
-  apiUrl: string = environment.apiurl;
-  token: any = localStorage.getItem('token');
-  apiHeadders: any;
-  constructor(private http: HttpClient, public router: Router) {
-    this.apiHeadders = {
-      Headers: new HttpHeaders({
-        'Authorization' : this.token,
-      }),
-    };
-  }
 
-  login(FormData:any): Observable<any> {
-    const apiPath = this.apiUrl + 'user-login';
-    return this.http.post(apiPath,FormData)
+  // apiUrl: string = environment.apiurl;
+  // token: any = localStorage.getItem('token');
+  // apiHeadders: any;
 
-  }
+  // constructor(private http: HttpClient, public router: Router) {
+  //   this.apiHeadders = {
+  //     Headers: new HttpHeaders({
+  //       'Authorization' : this.token,
+  //     }),
+  //   };
+  // }
+
+  // login(FormData:any): Observable<any> {
+  //   const apiPath = this.apiUrl + 'user-login';
+  //   return this.http.post(apiPath,FormData)
+
+  // }
+  
 }
