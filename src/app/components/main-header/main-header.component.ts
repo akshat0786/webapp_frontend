1921378a40e3b2e2446c7d70d990a0cc44f3d670
akshat <akshatroljack@gmail.com>
2024-01-08 18:12:26 +0530

    8 jan

--- src/app/components/main-header/main-header.component.ts
+++ src/app/components/main-header/main-header.component.ts
@@ -1,14 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import intlTelInput from 'intl-tel-input';
+import { ApiService } from '../../services/api.services';
+import { Router } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 declare var $: any;
 @Component({
   selector: 'app-main-header',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './main-header.component.html',
   styleUrl: './main-header.component.css',
 })
-export class MainHeaderComponent {}
+export class MainHeaderComponent {
+  constructor(public ApiService: ApiService, public router: Router) {
+    let token = localStorage.getItem('token');
+    if (token) {
+      this.router.navigate(['home']);
+    }
+  }
+
+  onSubmit(FormData: any) {
+    this.ApiService.login(FormData).subscribe({
+      next: (v) => {
+        console.log(v);
+        localStorage.setItem('userdata', JSON.stringify(v.user));
+        localStorage.setItem('token', v.token);
+        this.router.navigate(['home']);
+      },
+      error: (e) => {
+        console.error(e);
+      },
+      complete: () => console.info('complete'),
+    });
+  }
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   const phoneInput = document.getElementById('phone');
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
-
+  listItems = [
+    { name: 'Jaw Crusher', link: '#' },
+    { name: 'Stone Crusher', link: '#' },
+    { name: 'Cone Crusher', link: '#' },
+    { name: 'Mobile crusher', link: '#' },
+    { name: 'Impact Crusher', link: '#' },
+    { name: 'Roll crusher', link: '#' },
+    { name: 'VSI Crusher', link: '#' },
+    { name: 'HSI Crusher', link: '#' },
+    { name: 'Crusher', link: '#' },
+  ];
 }
